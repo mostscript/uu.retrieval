3c7df150b121abc3910bd30d82ea5550eeb0ea15
Sean Upton <sdupton@gmail.com>
2012-04-18 15:31:53 -0600

    initial interfaces.py with ISearchableSchema marker

---
+++ uu/retrieval/interfaces.py
@@ -0,0 +1,9 @@
+from zope.interface import Interface
+
+
+class ISearchableSchema(Interface):
+    """
+    Marker for an interface considered to be schema
+    for search consideration by this package.
+    """
+
