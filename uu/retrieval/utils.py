88fc7eb919c56755069bf13a667d672d30dc057c
Sean Upton <sdupton@gmail.com>
2012-05-16 17:38:08 -0600

    interface identification functions in utils

--- uu/retrieval/utils.py
+++ uu/retrieval/utils.py
@@ -1,6 +1,26 @@
+from hashlib import md5
+
+from plone.supermodel import serializeSchema
+from zope.interface.interfaces import IInterface
 
 # misc type check stuff:
 not_string = lambda v: not isinstance(v, basestring)
 iterable = lambda v: hasattr(v, '__iter__')
 is_multiple = lambda v: not_string(v) and iterable(v)
+
+
+signature = lambda iface: md5(serializeSchema(iface).strip()).hexdigest()
+
+
+def identify_dynamic_interface(iface):
+    name = 'I%s' % signature(iface)
+    return '.'.join((iface.__module__, name))
+
+
+def identify_interface(iface):
+    if not IInterface.providedBy(iface):
+        raise ValueError('Interface class not provided: %s' % repr(iface))
+    if iface.__name__:
+        return iface.__identifier__   # will have fully qualified dottedname
+    return identify_dynamic_interface(iface)  # fallback to identify dynamic
 
